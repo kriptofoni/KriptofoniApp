c93be8f4f932b7fba0ff0b5b596998d3fcdb8c0d
kriptofoni <info@kriptofoni.com>
2021-10-12 00:32:45 +0300

    İletişim Bilgileri

--- src/app/services/about.service.ts
+++ src/app/services/about.service.ts
@@ -12,6 +12,8 @@ export class AboutService {
         "titleImage": "../assets/imgs/decoLogo.png",
         "subtitle": "Kriptofoni, 2020 yılında Kriptofoni Teknoloji  Tic. Ltd. Şti. tarafından hayata geçirilmiş bir projedir. Dünya ve Türkiye gündeminde yer alan Bitcoin ve diğer kripto para haberlerini herkesin anlayacağı bir dil ile kullanıcılarına sunar. Sadece gerekli bilgileri vererek ziyaretçilerini kripto para dünyası hakkında bilgilendirmeyi amaç edinmiştir.",
         "adres": "Adres: Yeşilova Mahallesi 4023. Cadde Ser Tower Residence 1/29 Etimesgut/Ankara",
+        "e-posta": "E-Posta: info@kriptofoni.com",
+        "telefon": "Telefon: 0850 466 12 34",
         "about_text_button": "İletişime Geç",
         "about_text_button_link": "https://www.kriptofoni.com/iletisim-contact-us/",
         "items": [
